4193525ffdd3903ca070f8e9846b3eeb409930d6
AleshaPustota <4247334@mail.ru>
2025-10-14 13:48:19 +0300

    user update

--- user/user.go
+++ user/user.go
@@ -1,8 +1,24 @@
 package user
 
+import (
+	"errors"
+	"strings"
+)
+
+func (u user) NewUser(nickname string, password string, balance int, rating int) error {
+	if strings.TrimSpace(nickname) == "" {
+		return errors.New("nickname cannot be empty")
+	}
+	if len(password)<5{
+		return errors.New("password so short")
+	}
+	if balance<0
+}
+
 type user struct {
 	nickname string
 	password string
 	balance  int
 	rating   int
 }
+
