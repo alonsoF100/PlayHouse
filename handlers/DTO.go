eada34b8b9d7cbf027cb91f78193c3b0b54ec1bd
AleshaPustota <4247334@mail.ru>
2025-10-16 16:35:07 +0300

    dto

---
+++ handlers/DTO.go
@@ -0,0 +1,8 @@
+package handlers
+
+type UserHandlerInfo struct {
+	nickname string
+	password string
+	balance  int
+	rating   int
+}
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,10 +8,15 @@ import (
 )
 
 func HandlerNewUser(w http.ResponseWriter, r *http.Request) {
-	var user user.User
-	if err := json.NewDecoder(r.Body).Decode(user.NewUser); err != nil {
+	var dtoUser UserHandlerInfo
+	if err := json.NewDecoder(r.Body).Decode(&dtoUser); err != nil {
 		fmt.Println("err ", err)
 		return
 	}
-	fmt.Println(user)
+	user1, err := user.NewUser(dtoUser.nickname, dtoUser.password, dtoUser.balance, dtoUser.rating)
+	if err != nil {
+		fmt.Println("err ", err)
+		return
+	}
+	fmt.Println(user1)
 }
